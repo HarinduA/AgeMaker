4e7578e66a108b6b54fa9d108f608b2a9316b492
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-06-03 20:37:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your date of birth :");
+            string input = Console.ReadLine();
+            DateTime dob = DateTime.ParseExact(input, "yyyy-MM-dd", null);
+
+            int age = CalculateAge(dob);
+            Console.WriteLine("Your age is: " + age);
+
+            static int CalculateAge(DateTime dob)
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - dob.Year;
+
+                
+                int daysDifference = (today - dob).Days;
+
+                
+                age = daysDifference / 365;
+
+                return age;
+            }
+
+        }
+
+        
+    }
+}
